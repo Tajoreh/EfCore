a60abda00a4e7db50364c76e9dda28bfd61261eb
Tajoreh <tajoreh@gmail.com>
2023-10-08 01:15:53 +0330

    #adding some improvment in code

--- 02_Ef_Core_Performance/Program.cs
+++ 02_Ef_Core_Performance/Program.cs
@@ -12,5 +12,50 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+
+//Method 1: it takes 1001 updates for each  employee and also company
+app.MapPut("increase-salary", async (int companyId, DatabaseContext dbContext) =>
+{
+    var company = await dbContext
+        .Set<Company>()
+        .Include(x => x.Employees)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(c => c.Id == companyId);
+
+    if (company is null)
+        return Results.NotFound($"The company with id '{companyId}' was not found.");
+
+    foreach (var employee in company.Employees)
+    {
+        employee.Salary *= 1.1m;
+    }
+
+    company.LastSalaryUpdateUtc = DateTime.UtcNow;
+    await dbContext.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+
+app.MapPut("increase-salary-sql", async (int companyId, DatabaseContext dbContext) =>
+{
+    var company = await dbContext
+        .Set<Company>()
+        .AsNoTracking()
+        .FirstOrDefaultAsync(c => c.Id == companyId);
+
+    if (company is null)
+        return Results.NotFound($"The company with id '{companyId}' was not found.");
+
+    await dbContext.Database.ExecuteSqlInterpolatedAsync(
+         $"UPDATE Employees SET Salary= Salary * 1.1 WHERE companyId={companyId}"
+     );
+
+    company.LastSalaryUpdateUtc = DateTime.UtcNow;
+    await dbContext.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
 app.Run();
 
