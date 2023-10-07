081bd9a9575568d94fed793656da0f109fee03fd
tajoreh <tajoreh@gmail.com>
2023-10-07 15:18:59 +0330

    #add better way to read settings from appsettings

---
+++ 01_SampleUsage/Options/DatabaseOptions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace _01_SampleUsage.Options;
+
+public class DatabaseOptions
+{
+    public string ConnectionStrings { get; set; } = String.Empty;
+    public int MaxRetryCount { get; set; }
+    public int CommandTimeout { get; set; }
+    public bool EnableDetailedErrors { get; set; }
+    public bool EnableSensitiveDataLogging { get; set; }
+}
+
+public class DatabaseOptionSetup : IConfigureOptions<DatabaseOptions>
+{
+    readonly IConfiguration _configuration;
+
+    public DatabaseOptionSetup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Configure(DatabaseOptions options)
+    {
+        var connectionString = _configuration.GetConnectionString("Database");
+
+        options.ConnectionStrings = connectionString;
+
+        _configuration.GetSection(nameof(DatabaseOptions)).Bind(options);
+    }
+}
--- 01_SampleUsage/Program.cs
+++ 01_SampleUsage/Program.cs
@@ -1,32 +1,63 @@
 using _01_SampleUsage;
 using _01_SampleUsage.Models;
+using _01_SampleUsage.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Method 1: too many configurations hardCoded
+//builder.Services.AddDbContext<DatabaseContext>(
+//    dbContextOptionBuilder =>
+//    {
+//        var connectionString = builder.Configuration.GetConnectionString("database");
+
+//        dbContextOptionBuilder.UseSqlServer(connectionString, sqlServerActions =>
+//        {
+//            sqlServerActions.EnableRetryOnFailure(3);
+//            sqlServerActions.CommandTimeout(30);
+//        });
+
+
+//        //enable details
+//        //in development env and local
+//        dbContextOptionBuilder.EnableDetailedErrors(true);
+//        dbContextOptionBuilder.EnableSensitiveDataLogging(true);
+//    });
+
+//Method 2: Add Configurations to appsetting.json
+builder.Services.ConfigureOptions<DatabaseOptionSetup>();
+
+
 builder.Services.AddDbContext<DatabaseContext>(
-    dbContextOptionBuilder =>
-    {
-        var connectionString = builder.Configuration.GetConnectionString("database");
+   (serviceProvider, dbContextOptionBuilder) =>
+   {
+       var databaseOptions = serviceProvider.GetService<IOptions<DatabaseOptions>>().Value;
 
-        dbContextOptionBuilder.UseSqlServer(connectionString);
-    });
+       dbContextOptionBuilder.UseSqlServer(databaseOptions.ConnectionStrings, sqlServerActions =>
+       {
+           sqlServerActions.EnableRetryOnFailure(databaseOptions.MaxRetryCount);
+           sqlServerActions.CommandTimeout(databaseOptions.CommandTimeout);
+       });
 
+       dbContextOptionBuilder.EnableDetailedErrors(databaseOptions.EnableDetailedErrors);
+       dbContextOptionBuilder.EnableSensitiveDataLogging(databaseOptions.EnableSensitiveDataLogging);
+   });
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("companies/{companyId:int}",async  (int companyId,DatabaseContext context) =>
+app.MapGet("companies/{companyId:int}", async (int companyId, DatabaseContext context) =>
 {
-    var company=await context
+    var company = await context
         .Set<Company>()
         .AsNoTracking()
-        .FirstOrDefaultAsync(c=>c.Id == companyId);
+        .FirstOrDefaultAsync(c => c.Id == companyId);
 
     if (company is null)
         return Results.NotFound($"The company with id '{companyId}' was not found.");
 
-    var response=new CompanyResponse(company.Id,company.Name);
+    var response = new CompanyResponse(company.Id, company.Name);
 
     return Results.Ok(response);
 });
